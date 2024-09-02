e6af5decf389a9685394caebbce178ff79c38cb6
keerthisureka <keerthisureka03@gmail.com>
2024-09-02 23:02:04 +0530

    done

---
+++ utils/footer.php
@@ -0,0 +1,9 @@
+<hr class="footerline">
+
+<footer>
+    <div class="container">
+        <h6 style="text-align:center;">
+            Copyright © <?php echo date("Y"); ?> 
+        </h6>
+    </div>
+</footer>
